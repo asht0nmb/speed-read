1d6fe16d86c41640c6f4d107d7106b2d0c4e917f
ashtonmb <ashtonmb@uw.edu>
2026-02-26 18:27:53 -0800

    Add Vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import SpeedReader from './SpeedReader'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <SpeedReader />
+    <Analytics />
   </StrictMode>
 )
