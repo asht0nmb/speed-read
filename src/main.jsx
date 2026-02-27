aacedd38ac2814c6c8ae35975c9b9c9d5eb59643
ashtonmb <ashtonmb@uw.edu>
2026-02-26 23:02:30 -0800

    Update vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Analytics } from '@vercel/analytics/react'
 import SpeedReader from './SpeedReader'
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <SpeedReader />
     <Analytics />
+    <SpeedInsights/>
   </StrictMode>
 )
